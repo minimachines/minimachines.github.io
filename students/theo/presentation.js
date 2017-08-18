0fc25fde4fa4b545d150ca9779c36200b07e026a
minimachines <30911798+minimachines@users.noreply.github.com>
2017-08-18 11:28:18 -0700

    final

--- students/theo/presentation.js
+++ students/theo/presentation.js
@@ -2,26 +2,27 @@ window.onload = function() {
 	document.getElementById("pizza").onclick = fun1;
     document.getElementById("magic eight ball").onclick = fun2;
     document.getElementById("cookie").onclick = fun3;
-    document.getElementById("director X").onclick = fun4;   document.getElementById("game").onclick = fun5;
+    document.getElementById("director X").onclick = fun4;   
+    document.getElementById("game").onclick = fun5;
 
 }
 
 function fun1() {
-	window.open("file:///C:/Users/iD%20Student/Desktop/Theo%20M/pizza.html");
+	window.open("pizza.html");
 }
 
 function fun2() {
-	window.open("file:///C:/Users/iD%20Student/Desktop/Theo%20M/magic.html");
+	window.open("magic.html");
 }
 
 function fun3() {
-	window.open("file:///C:/Users/iD%20Student/Desktop/Theo%20M/cookie.html");
+	window.open("cookie.html");
 }
 
 function fun4() {
-	window.open("file:///C:/Users/iD%20Student/Desktop/Theo%20M/Director%20X.html");
+	window.open("Director X.html");
 }
 
 function fun5() {
-	window.open("file:///C:/Users/iD%20Student/Desktop/Theo%20M/game.html");
+	window.open("game.html");
 }
